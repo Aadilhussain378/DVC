c2ed51ea640c69486f3effea24e459a95c1e703c
Aadil Hussain <hussainaadil378@gmail.com>
2023-12-11 22:44:10 +0530

    Forth comit

--- test.py
+++ test.py
@@ -2,9 +2,11 @@ import pandas as pd
 
 data=[
 
-    {"name":"sunny","age":27,"city":"bhopal"}
-    {"name":"krish","age":37,"city":"bangluru"}
-    {"name":"Aadil","age":23,"city":"Anantnag"}
+    {"name":"sunny","age":27,"city":"bhopal"},
+    {"name":"krish","age":37,"city":"bangluru"},
+    {"name":"Aadil","age":23,"city":"Anantnag"},
+    {"name":"AKther","age":27,"city":"Kokernag"}
+
 
 ]
 
